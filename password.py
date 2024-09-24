89225acd4970ffa30ba3bc199bdacbff3ab842e1
Koenyyrtc <koenc.coding@gmail.com>
2024-09-24 19:01:04 +0100

    Update password.py

--- password.py
+++ password.py
@@ -1,1 +1,6 @@
+import string
+import random
 
+def generate_password(length=8):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
